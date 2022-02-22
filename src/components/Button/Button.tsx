e55e443387918bcbdd5731123a4114fd51797eb1
Alix Dritlein <alixdritlein@macs-air.lan>
2022-02-21 19:41:57 -0700

    added the stitches theme

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,20 @@
-import {styled} from '@stitches/react'
+import {styled} from '../../theme/stitches.config';
 
 const Button = styled('button', {
   appearance: 'none',
   border: 'none',
   display: 'inline-block',
   fontWeight: '500',
-  lineHeight: '14px',
+  lineHeight: '$4',
   textAlign: 'center',
   verticalAlign: 'middle',
   cursor: 'pointer',
   WebkitUserSelect: 'none',
   MozUserSelect: 'none',
   userSelect: 'none',
-  padding: '.5rem 1rem',
+  p: '$1 $4',
   fontSize: '15px',
-  borderRadius: '4px',
+  br: '4px',
 })
 
 export default Button;
